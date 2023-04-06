e0f07fe6b922cd2df5cf2e4a00ee482da3530c16
GuidoBonesi <guidobonesi@gmail.com>
2023-04-06 12:24:20 -0300

    feat: redux user + bugfix navbar

--- src/commons/Grid/Grid.jsx
+++ src/commons/Grid/Grid.jsx
@@ -4,7 +4,6 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./Grid.css";
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { Button, Card, Form, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { BiBed, BiBath, BiPhoneCall } from "react-icons/bi";
@@ -13,85 +12,132 @@ import { MdFavoriteBorder } from "react-icons/md";
 import { SlLocationPin } from "react-icons/sl";
 
 export const Grid = () => {
-  const [properties, setProperties] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("/api/property/")
-      .then((res) => {
-        setProperties(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
   return (
     <section>
-      <Container className="cont-grid">
+      <Container style={{ width: "100%", color: "#123AC8" }}>
         <Row className="row-grid">
-          {properties.map((propiedad) => {
-            return (
-              <Col className="col-grid col-md-offset-2" sm={6}>
-                <Card.Body>
-                  <Row>
-                    <Col className="col-grid" sm={12}>
-                      <Card.Title>{propiedad.title}</Card.Title>
-                    </Col>
-                    <Col className="col-grid" sm={6}>
-                      <Form.Text className="text-muted">
-                        $ {propiedad.price}
-                      </Form.Text>
-                    </Col>
-                    <Col className="col-grid" sm={6}>
-                      <Form.Text className="text-muted">
-                        <SlLocationPin /> {propiedad.city}
-                      </Form.Text>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="col-grid" sm={4}>
-                      <Form.Text className="text-muted">
-                        <RxRulerSquare /> {propiedad.area} m2
-                      </Form.Text>
-                    </Col>
+          <Col xs={12} md={6} lg={6} style={{ padding: "1.6%" }}>
+            <Card
+              style={{
+                height: "110%",
+                border: "1px solid #123AC8",
+                borderRadius: "0px",
+                marginLeft: "15%",
+              }}
+            >
+              <Row className="row-grid">
+                <Col xs={5}>
+                  <Card.Img
+                    style={{
+                      height: "110%",
+                      borderRight: "1px solid #123AC8",
+                      padding: "2%",
+                      marginLeft: "5%",
+                      borderRadius: "0%",
+                    }}
+                  />
+                </Col>
+                <Col xs={7}>
+                  <Card.Body
+                    style={{
+                      padding: "0%",
 
-                    <Col className="col-grid" sm={4}>
-                      <Form.Text className="text-muted">
-                        <BiBed /> {propiedad.bedroom} Dorm.
-                      </Form.Text>
-                    </Col>
-                    <Col className="col-grid" sm={4}>
-                      <Form.Text className="text-muted">
-                        <BiBath className="botones-info" /> {propiedad.bathroom}{" "}
+                      height: "110%",
+                    }}
+                  >
+                    <div style={{ width: "100%", display: "flex" }}>
+                      <div
+                        style={{
+                          width: "40%",
+                          padding: "3%",
+                          display: "flex",
+                          alignItems: "center",
+                          fontSize: "12px",
+                          border: "1px solid #123AC8",
+                        }}
+                      >
+                        Precio
+                      </div>
+                      <div
+                        style={{
+                          width: "60%",
+                          fontSize: "11px",
+                          padding: "3%",
+                          display: "flex",
+                          alignItems: "center",
+                          border: "1px solid #123AC8",
+                        }}
+                      >
+                        Ubicacion
+                      </div>
+                    </div>
+                    <div style={{ display: "flex" }}>
+                      <div
+                        style={{
+                          width: "34%",
+                          padding: "3%",
+                          display: "flex",
+                          alignItems: "center",
+                          fontSize: "13px",
+                          border: "1px solid #123AC8",
+                        }}
+                      >
+                        Area
+                      </div>
+                      <div
+                        style={{
+                          width: "33%",
+                          padding: "2%",
+                          display: "flex",
+                          alignItems: "center",
+                          fontSize: "13px",
+                          border: "1px solid #123AC8",
+                        }}
+                      >
+                        Bedrrom
+                      </div>
+
+                      <div
+                        style={{
+                          width: "33%",
+                          padding: "2%",
+                          display: "flex",
+                          alignItems: "center",
+                          fontSize: "13px",
+                          border: "1px solid #123AC8",
+                        }}
+                      >
                         Baños
-                      </Form.Text>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="col-grid" sm={12}>
-                      <Form.Text className="text-muted">
-                        {propiedad.description.length > 250
-                          ? `${propiedad.description.substring(0, 250)}...`
-                          : propiedad.description}
-                      </Form.Text>
-                    </Col>
-                  </Row>
-                </Card.Body>
-                <Card.Body className="botones-div">
-                  <Button className="botones-cta">
-                    <MdFavoriteBorder className="boton-cta" />
-                  </Button>
-                  <Button className="botones-cta">
-                    <BiPhoneCall className="boton-cta" />
-                  </Button>
-                  <Link to={`/property/${propiedad.id}`}>
-                    <Button className="buttonVerMas">VER MÁS</Button>
-                  </Link>
-                </Card.Body>
-              </Col>
-            );
-          })}
+                      </div>
+                    </div>
+                    <Card.Text
+                      style={{
+                        padding: "5%",
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      Descrip
+                    </Card.Text>
+                    <div
+                      style={{
+                        borderTop: "1px solid #123AC8",
+                        height: "25%",
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div style={{ marginLeft: "30%" }}></div>
+                      <div style={{ marginLeft: "8%" }}></div>
+
+                      <button>Ver más</button>
+                    </div>
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
         </Row>
       </Container>
     </section>
